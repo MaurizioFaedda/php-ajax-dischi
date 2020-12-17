5204d07def55f2f0d25e183d1cba868502ef8065
Maurizio <maurizio.faedda1@gmail.com>
2020-12-17 17:18:46 +0100

    Printed card disco, used handlebars, jquery and ajax

--- dist/app.js
+++ dist/app.js
@@ -93,7 +93,26 @@
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-
+$(document).ready(function () {
+  var source = $("#disco-template").html();
+  var template = Handlebars.compile(source);
+  $.ajax({
+    url: '../database/dischi.php',
+    method: 'GET',
+    success: function success(data) {
+      for (var i = 0; i < data.length; i++) {
+        var context = {
+          poster: data[i].poster,
+          title: data[i].title,
+          author: data[i].author,
+          year: data[i].year
+        };
+        var html = template(context);
+        $('.dischi-box').append(html);
+      }
+    }
+  });
+});
 
 /***/ }),
 
